246dd1931beb0a7c59e7acb5a95c155b39f55ccc
Zane <i.zane.am.robot@gmail.com>
2018-09-05 21:37:53 -0400

    Adds some database seeds, comments out normalize css, sets a default url

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+polyglot_confessions_survey = Survey.create!(
+  title: "Polyglot Confessions Survey",
+  description: "An anonymous survey for polyglot community members."
+)
+
+question_1 = Question.create!(
+  title: "Confess anything!",
+  description: "Pour your heart out to the polyglot community.
+  Please do not submit abusive or bullying content.",
+  survey: polyglot_confessions_survey
+)
