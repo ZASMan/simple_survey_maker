5f5a5371f0336890e9437e1b1281490a149ccdee
Zane <i.zane.am.robot@gmail.com>
2018-09-05 21:22:45 -0400

    Runs migration for answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_011935) do
+ActiveRecord::Schema.define(version: 2018_09_06_012208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.bigint "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer "priority", default: 0, null: false
@@ -46,5 +54,6 @@ ActiveRecord::Schema.define(version: 2018_09_06_011935) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "questions", "surveys"
 end
