3aeacecdefbef2e322dfa5d4fe3bb3811443ba76
Zane <i.zane.am.robot@gmail.com>
2018-09-07 23:06:57 -0400

    Adds note to answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,8 @@
 class Answer < ApplicationRecord
   belongs_to :question
   validates :body, presence: true, length: { maximum: 2000 }
+
+  def evaluate_body_content
+  	# Evaluate content here
+  end
 end
