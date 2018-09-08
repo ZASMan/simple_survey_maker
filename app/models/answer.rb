a4b8cee3c76ba35c1ab1315fd7985b30f407bddb
Zane <i.zane.am.robot@gmail.com>
2018-09-07 23:16:35 -0400

    Fixes typo in answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
-  has_many :comments, as: :commentableil
+  has_many :comments, as: :commentable
   validates :body, presence: true, length: { maximum: 2000 }
 
   ANSWER_STATUSES = %w[flagged posted]
