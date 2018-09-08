cdb2ca3210e48b7441a435bf3f423f5f7f00b30c
Zane <i.zane.am.robot@gmail.com>
2018-09-07 23:15:53 -0400

    Added statuses to answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,9 @@
 class Answer < ApplicationRecord
   belongs_to :question
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentableil
   validates :body, presence: true, length: { maximum: 2000 }
+
+  ANSWER_STATUSES = %w[flagged posted]
 
   def evaluate_body_content
   	# Evaluate content here
