5812524d7973ccae11f779818acb118c66f40c93
Zane <i.zane.am.robot@gmail.com>
2018-09-09 18:54:47 -0400

    SMTP config

--- config/smtp.rb
+++ config/smtp.rb
@@ -3,7 +3,7 @@ SMTP_SETTINGS = {
   authentication: :plain,
   domain: 'heroku.com', # example: "heroku.com"
   enable_starttls_auto: true,
-  password: ENV.fetch("SMTP_PASSWORD"),
+  password: 'password',
   port: "587",
-  user_name: ENV.fetch("SMTP_USERNAME")
+  user_name: 'username'
 }
