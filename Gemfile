b826bd8f311860ab34cd2a9f3de22dd61598c02f
Amanda Rolon <abrolon87@gmail.com>
2021-03-04 13:10:58 -0500

    updates gems; resets db

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-ruby "2.7.1"
+ruby "2.7.0"
 
 gem 'obscenity'
 gem "autoprefixer-rails"
--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2018_09_14_011356) do
   end
 
   create_table "content_filters", force: :cascade do |t|
-    t.text "filter_list"
+    t.text "filter_list", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
